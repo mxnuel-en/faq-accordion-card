7a97e4f74240610172438a6867b67e0cb12a1042
Emmanuel Ngurukwem <manuelenweremchi@gmail.com>
2023-11-18 22:00:09 +0100

    first commit (not done)

---
+++ src/App.tsx
@@ -0,0 +1,142 @@
+import { useState } from "react";
+import { qaList } from "./faqQA";
+
+interface ListProps {
+  questions: {
+    id: string;
+    question: string;
+    answer: string;
+  }[];
+  onSetOpenAccordion: (id: string) => void;
+  openAccordion: {
+    [id: string]: boolean;
+  };
+}
+
+interface ListItemsProps {
+  question: string;
+  answer: string;
+  openAccordion: boolean;
+}
+
+function App() {
+  const [openAccordion, setOpenAccordion] = useState<{ [id: string]: boolean }>(
+    {}
+  );
+
+  const handleOpenAccordion = (id: string) => {
+    setOpenAccordion((prevState) => {
+      const newOpenAccordion = { ...prevState };
+      newOpenAccordion[id] = !prevState[id]; // Update the state of the clicked accordion
+
+      // Close all other accordions
+      for (const key in newOpenAccordion) {
+        if (key !== id) {
+          newOpenAccordion[key] = false;
+        }
+      }
+
+      return newOpenAccordion;
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="hero">
+        <div className="hero_illustration">
+          <picture>
+            <source
+              media="(max-width: 1023px)"
+              srcSet="images/illustration-woman-online-mobile.svg"
+            />
+            <img
+              className="hero_illustration--image"
+              src="./images/illustration-woman-online-desktop.svg"
+              alt="illustration woman online"
+            />
+          </picture>
+          <picture>
+            <source
+              media="(max-width: 1023px)"
+              srcSet="images/bg-pattern-mobile.svg"
+            />
+            <img
+              className="hero_illustration--shadow"
+              src="./images/bg-pattern-desktop.svg"
+              alt="background pattern"
+            />
+          </picture>
+        </div>
+
+        <img
+          className="hero_illustration--box"
+          src="images/illustration-box-desktop.svg"
+          alt="box illustration"
+        />
+      </div>
+
+      <QAList
+        questions={qaList}
+        onSetOpenAccordion={handleOpenAccordion}
+        openAccordion={openAccordion}
+      />
+    </div>
+  );
+}
+
+function QAList({ questions, onSetOpenAccordion, openAccordion }: ListProps) {
+  return (
+    <main>
+      <h2 className="title">faq</h2>
+      <ul>
+        {questions.map((qa) => (
+          <li
+            key={qa.id}
+            className="faq"
+            onClick={() => onSetOpenAccordion(qa.id)}
+          >
+            <QuestionAnswerItem
+              question={qa.question}
+              answer={qa.answer}
+              openAccordion={openAccordion[qa.id]}
+            />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function QuestionAnswerItem({
+  question,
+  answer,
+  openAccordion,
+}: ListItemsProps) {
+  return (
+    <>
+      <div className="question">
+        <h3>{question}</h3>
+
+        <svg
+          className={`arrow${openAccordion ? "--active" : ""}`}
+          width="10"
+          height="7"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M1 .799l4 4 4-4"
+            stroke="#F47B56"
+            strokeWidth="2"
+            fill="none"
+            fillRule="evenodd"
+          />
+        </svg>
+      </div>
+      <div className={`answer_container${openAccordion ? "--active" : ""}`}>
+        <p className="answer_content">{answer}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/faqQA.ts
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const qaList = [
+    {
+        id: uuidv4(),
+        question: "How many team members can I invite?",
+        answer: "The maximum number of team members you can invite depends on your plan. Please refer to our pricing page to view the specific team member limits for each plan."
+    },
+    {
+        id: uuidv4(),
+        question: "What is the maximum file upload size?",
+        answer: "No more than 2GB. All files in your account must fit your allotted storage space."
+    },
+    {
+        id: uuidv4(),
+        question: "How do I reset my password?",
+        answer: "Go to the login page and click on \"Forgot Password\" and follow the instructions to reset your password."
+    },
+    {
+        id: uuidv4(),
+        question: "Can I cancel my subscription?",
+        answer: "You can cancel your subscription at any time. Just go to your account page and click on \"Your Subscriptions\" tab where you will find \"Cancel Subscription\"."
+    },
+    {
+        id: uuidv4(),
+        question: "Do you provide additional support?",
+        answer: "We offer a variety of support options to help you get the most out of our products and services."
+    }
+]
